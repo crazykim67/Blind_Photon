5df2db29bd8c0301d7e281da3d8eb09e2283e94c
crazykim67 <crazykim67@gmail.com>
2024-02-18 05:52:32 +0900

    씬 수정

--- Assets/Assets/00. Script/MainMenu/MainMenuController.cs
+++ Assets/Assets/00. Script/MainMenu/MainMenuController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,9 @@ public class MainMenuController : MonoBehaviour
     public enum State
     {
         Main,
+        HostandJoin,
         Host,
+        Join,
     }
 
     [Header("Main UI Group")]
@@ -26,7 +29,7 @@ public class MainMenuController : MonoBehaviour
     [Header("Host/Join UI Group")]
 
     [SerializeField]
-    private GameObject hostGroup;
+    private GameObject hostJoinGroup;
 
     [SerializeField]
     private Button hostBtn;
@@ -35,6 +38,30 @@ public class MainMenuController : MonoBehaviour
     [SerializeField]
     private Button backBtn;
 
+    [Header("Host UI Group")]
+    [SerializeField]
+    private GameObject hostGroup;
+    [SerializeField]
+    private Button hostBackBtn;
+    [SerializeField]
+    private Button decreaseBtn;
+    [SerializeField]
+    private Button increaseBtn;
+    [SerializeField]
+    private TextMeshProUGUI countText;
+    [SerializeField]
+    private int playerCount = 2;
+
+    [Header("Join UI Group")]
+    [SerializeField]
+    private GameObject joinGroup;
+    [SerializeField]
+    private TMP_InputField codeInputField;
+    [SerializeField]
+    private Button roomJoinBtn;
+    [SerializeField]
+    private Button joinBackBtn;
+
     [Header("Exit Group")]
     [SerializeField]
     private GameObject exitGroup;
@@ -42,13 +69,20 @@ public class MainMenuController : MonoBehaviour
     private Button exitYesBtn;
     [SerializeField]
     private Button exitNoBtn;
+    
 
     private void Awake()
     {
-        startBtn.onClick.AddListener(() => { OnChange(State.Host); });
+        startBtn.onClick.AddListener(() => { OnChange(State.HostandJoin); });
         backBtn.onClick.AddListener(() => { OnChange(State.Main); });
-        exitBtn.onClick.AddListener(() => { exitGroup.SetActive(true); });
 
+        hostBtn.onClick.AddListener(() => { OnChange(State.Host); });
+        hostBackBtn.onClick.AddListener(() => { OnChange(State.HostandJoin); });
+
+        joinBtn.onClick.AddListener(() => { OnChange(State.Join); });
+        joinBackBtn.onClick.AddListener(() => { OnChange(State.HostandJoin); });
+
+        exitBtn.onClick.AddListener(() => { exitGroup.SetActive(true); });
         exitYesBtn.onClick.AddListener(() => 
         {
 #if UNITY_EDITOR
@@ -68,15 +102,58 @@ public class MainMenuController : MonoBehaviour
             case State.Main:
                 {
                     mainGroup.SetActive(true);
+                    hostJoinGroup.SetActive(false);
+                    break;
+                }
+            case State.HostandJoin:
+                {
+                    mainGroup.SetActive(false);
+                    hostJoinGroup.SetActive(true);
                     hostGroup.SetActive(false);
+                    joinGroup.SetActive(false);
                     break;
                 }
             case State.Host:
                 {
-                    mainGroup.SetActive(false);
+                    hostJoinGroup.SetActive(false);
                     hostGroup.SetActive(true);
+
+                    PlayerCountInit();
+                    break;
+                }
+            case State.Join:
+                {
+                    hostJoinGroup.SetActive(false);
+                    joinGroup.SetActive(true);
+
+                    JoinInit();
                     break;
                 }
         } 
     }
+
+    #region PlayerCount
+
+    private void PlayerCountInit()
+    {
+        playerCount = 2;
+        countText.text = playerCount.ToString();
+    }
+
+    public void OnPlayerCountChanged(bool isPlus)
+    {
+        playerCount = Mathf.Clamp(playerCount + (isPlus ? 1 : -1), 1, 4);
+        countText.text = playerCount.ToString();
+    }
+
+    #endregion
+
+    #region Join
+
+    private void JoinInit()
+    {
+        codeInputField.text = "";
+    }
+
+    #endregion
 }
