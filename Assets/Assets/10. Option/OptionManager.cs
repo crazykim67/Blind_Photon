89e8531f3a478c9f3cfadb44a9b7588d11b577cf
crazykim67 <crazykim67@gmail.com>
2024-02-21 08:20:01 +0900

    옵션 메뉴 구현 중

--- Assets/Assets/10. Option/OptionManager.cs
+++ Assets/Assets/10. Option/OptionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class OptionManager : MonoBehaviour
@@ -34,5 +35,15 @@ public class OptionManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+    }
+
+    public void OnShow()
+    {
+        
+    }
+
+    public void OnHide()
+    {
+
     }
 }
