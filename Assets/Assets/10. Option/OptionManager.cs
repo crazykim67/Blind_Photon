a0574cc844d9ab6d2177e5768d8ff0d2dbbc6f27
crazykim67 <crazykim67@gmail.com>
2024-02-21 20:06:49 +0900

    수정

--- Assets/Assets/10. Option/OptionManager.cs
+++ Assets/Assets/10. Option/OptionManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using static UnityEditor.Progress;
 using System;
 
 public class OptionManager : MonoBehaviour
